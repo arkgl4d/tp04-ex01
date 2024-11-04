d85a31348116ee0636b3626851d383826b25e442
arkgl4d <101040264+arkgl4d@users.noreply.github.com>
2024-11-04 15:16:54 +0100

    commit

---
+++ local.tf
@@ -0,0 +1,9 @@
+locals {
+  user = "lgouasquet"             # Change this to your own username
+  tp   = basename(abspath(path.root)) # Get the name of the current directory
+  name = "${local.user}-${local.tp}"  # Concatenate the username and the directory name
+  tags = {                            # Define a map of tags to apply to all resources
+    Name  = local.name
+    Owner = local.user
+  }
+}
---
+++ vpc.tf
@@ -0,0 +1,39 @@
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "eu-north-1a"
+}
+
+resource "aws_subnet" "public_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.2.0/24"
+  availability_zone = "eu-north-1b"
+}
+
+resource "aws_subnet" "public_3" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "eu-north-1c"
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.101.0/24"
+  availability_zone = "eu-north-1a"
+}
+
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.102.0/24"
+  availability_zone = "eu-north-1b"
+}
+
+resource "aws_subnet" "private_3" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.103.0/24"
+  availability_zone = "eu-north-1c"
+}
