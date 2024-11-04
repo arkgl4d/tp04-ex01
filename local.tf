ba4b8ed3aad8c39592bd063628c53f078d0542e9
arkgl4d <101040264+arkgl4d@users.noreply.github.com>
2024-11-04 15:21:32 +0100

    commit

--- local.tf
+++ local.tf
@@ -1,5 +1,5 @@
 locals {
-  user = "lgouasquet"             # Change this to your own username
+  user = "lgouasquet"                 # Change this to your own username
   tp   = basename(abspath(path.root)) # Get the name of the current directory
   name = "${local.user}-${local.tp}"  # Concatenate the username and the directory name
   tags = {                            # Define a map of tags to apply to all resources
